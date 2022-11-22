8efcbb28aee04d21b452e38a3182f5d4b5e43cf1
Kenneth Laskoski <kenneth@laskoski.com.br>
2022-11-21 23:34:02 -0300

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Kenneth Laskoski on 21/11/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
